8fc2f3fdf4934044065b67af7c962d0bcce752b9
Kelsey <kelsey@lovedlockedloaded.com>
2021-04-28 06:02:22 +0000

    DRY multi-card updates

--- template-parts/kiss/flexi-partials/multi-block-layout.php
+++ template-parts/kiss/flexi-partials/multi-block-layout.php
@@ -1,4 +1,4 @@
-<?php
+<?
 	// GENERAL INIT
 	$templatePath = get_template_directory();
 	$sepPrefix = 'block';
@@ -20,65 +20,15 @@
 	$blockButtonColor = get_sub_field('block_button_color');
 	$blockButtonSize = get_sub_field('block_button_size');
 	
-	/// TITLES CONTROLS (Individual Cards)
-	$titlesTextSize = get_sub_field('block_titles_controls_font_size');
-	$titlesTextColor = get_sub_field('block_titles_controls_font_color');
-	$titlesTextWeight = get_sub_field('block_titles_controls_font_weight');
-	$titlesTextAlignment = get_sub_field('block_titles_controls_text_alignment');
-	$titlesTextClass = $titlesTextSize . ' ' . $titlesTextColor . ' ' . $titlesTextWeight . ' ' . $titlesTextAlignment;
 	
-	/// CONTENT TEXT CONTROLS (Individual Cards)
-	$contentTextSize = get_sub_field('content_text_controls_font_size');
-	$contentTextColor = get_sub_field('content_text_controls_font_color');
-	$contentTextWeight = get_sub_field('content_text_controls_font_weight');
-	$contentTextAlignment = get_sub_field('content_text_controls_text_alignment');
-	$contentTextClass = $contentTextSize . ' ' . $contentTextColor . ' ' . $contentTextWeight . ' ' . $contentTextAlignment;
-	
-	
-	
+	// GET BUTTONS
 	$addButton = false;
 	if(get_sub_field($sepPrefix . '_button_add_button') == true):
-		$addButton = true;
-	endif;
-	
-	$linkContent = '';
-	$linkType = '';
-	$externalLink = false;
-	$mailto = false;
-	$setLink = 'internal';
-	$linkClass = '';
-	if(get_sub_field($sepPrefix . '_button_button_options')):
-		$buttonData = get_sub_field($sepPrefix . '_button_button_options');
-		$linkType = $buttonData['button_link_type'];
-		$linkText = $buttonData['button_text_copy'];
-		//$linkType = $linkType['cta_button_link'];
-		switch ($linkType) {
-			case "page":
-				$setLink = 'page';
-				$linkContent = $buttonData['button_page_link'];
-				break;
-			case "link":
-				$setLink = 'link';
-				$linkContent = $buttonData['button_external_link'];
-				$externalLink = true;
-				break;
-			case "email":
-				$setLink = 'email';
-				$linkContent = $buttonData['button_email_address'];
-				$mailto = true;
-				break;
-			case "form":
-				$setLink = 'form';
-				$linkContent = '#';
-				$linkClass = $buttonData['button_popup'];
-				break;
-		}
-		
+		include $templatePartials . "buttons.php";
 	endif;
 	
 	/// SEPARATORS INIT
-	$separatorLayout = $templatePath . "/template-parts/kiss/static-partials/separators.php";
-	
+	$separatorLayout = $templatePartials . "separators.php";
 	include $separatorLayout;
 	
 	/// BLOCK LAYOUTS
@@ -125,40 +75,37 @@
 	$blockCount = get_sub_field('block_category_posts_num');
 	
 	
-	/// DIRECTION
-	$containerDirection = 'text-' . get_sub_field($sepPrefix . '_container_direction');
-	
 ?>
-<section id="multiBlock" class="multi-block <?php echo $bgcolour . ' ' . $separatorClasses . ' style-' . $blockAR . ' ' . $colOrder . ' ' . $containerDirection; ?> <?php if(get_sub_field('cta_background_image')):?>hasBgImg<?php endif; ?>" style="<?php if(get_sub_field('cta_background_image')):?>background-image:url(<?php  the_sub_field('cta_background_image'); ?>)<?php endif; ?>;">
-	<?php if($addSeparatorUpper == true):
+<section id="multiBlock" class="multi-block <? echo $bgcolour . ' ' . $separatorClasses . ' style-' . $blockAR . ' ' . $colOrder . ' ' . $containerDirection; ?> <? if(get_sub_field('cta_background_image')):?>hasBgImg<? endif; ?>" style="<? if(get_sub_field('cta_background_image')):?>background-image:url(<?  the_sub_field('cta_background_image'); ?>)<? endif; ?>;">
+	<? if($addSeparatorUpper == true):
 		include $pathUpper;
 	endif; ?>
 	<div class="multi-block__wrap flexi-inner">
-		<?php if($blockContainer):?><div class="container"><?php endif; ?>
-			<?php if(!empty($blockTitle)):?><h2 class="<?= $titleTextClass; ?>"><?php echo $blockTitle; ?></h2><?php endif; ?>
-			<?php if(!empty($blockIntro)):?>
-				<div class="multi-block__intro <?= $introTextClass; ?>"><?php echo $blockIntro; ?></div>
-			<?php endif; ?>
+		<? if($blockContainer):?><div class="container"><? endif; ?>
+			<? if(!empty($blockTitle)):?><h2 class="<?= $titleTextClass; ?>"><? echo $blockTitle; ?></h2><? endif; ?>
+			<? if(!empty($blockIntro)):?>
+				<div class="multi-block__intro <?= $introTextClass; ?>"><? echo $blockIntro; ?></div>
+			<? endif; ?>
 			
-			<?php if($blockType == 'manual'){ ?>
-			<?php if( have_rows('block_post_select_repeater') ): ?>
-			<div class="row <?php if($blockGutters):?>no-gutters<?php endif; ?>">
-				<?php while( have_rows('block_post_select_repeater') ): the_row(); 
+			<? if($blockType == 'manual'){ ?>
+			<? if( have_rows('block_post_select_repeater') ): ?>
+			<div class="row <? if($blockGutters):?>no-gutters<? endif; ?>">
+				<? while( have_rows('block_post_select_repeater') ): the_row(); 
 					$selectedItem  = get_sub_field('block_page_select');
 				?>
-				<div class="col-12 col-md-<?php echo $blockColumns; ?> multi-block__column">
-					<div class="multi-block__wrap-inner <?php echo $blockStyle . ' ' . $blockBGColor; ?>">
-						<?php include $templatePath . '/template-parts/kiss/flexi-partials/multi-block-layouts/block-' . $blockStyle . '.php'; ?>
+				<div class="col-12 col-md-<? echo $blockColumns; ?> multi-block__column">
+					<div class="multi-block__wrap-inner <? echo $blockStyle . ' ' . $blockBGColor; ?>">
+						<? include $templatePath . '/template-parts/kiss/flexi-partials/multi-block-layouts/block-' . $blockStyle . '.php'; ?>
 					</div>
 				</div>
-				<?php endwhile; ?>
+				<? endwhile; ?>
 			</div>
-			<?php endif; ?>
-			<?php 
+			<? endif; ?>
+			<? 
 				
 				} elseif($blockType == 'category'){?>
 			
-			<?php 
+			<? 
 				
 				$latestPostCategories = implode(', ', get_sub_field('block_get_latest_posts'));
 				
@@ -173,24 +120,24 @@
 				$arr_posts = new WP_Query( $args );
 				 
 				if ( $arr_posts->have_posts() ) : ?>
-				<div class="row <?php if($blockGutters):?>no-gutters<?php endif; ?>">
-				    <?php while ( $arr_posts->have_posts() ) :
+				<div class="row <? if($blockGutters):?>no-gutters<? endif; ?>">
+				    <? while ( $arr_posts->have_posts() ) :
 				        $arr_posts->the_post();
 				        $postID = get_the_ID();
 				        $selectedItem = get_post($postID);
 				        ?>
-				        <div class="col-12 col-md-<?php echo $blockColumns; ?> multi-block__column">
-							<div class="multi-block__wrap-inner <?php echo $blockStyle . ' ' . $blockBGColor; ?>">
-								<?php include $templatePath . '/template-parts/kiss/flexi-partials/multi-block-layouts/block-' . $blockStyle . '.php'; ?>
+				        <div class="col-12 col-md-<? echo $blockColumns; ?> multi-block__column">
+							<div class="multi-block__wrap-inner <? echo $blockStyle . ' ' . $blockBGColor; ?>">
+								<? include $templatePath . '/template-parts/kiss/flexi-partials/multi-block-layouts/block-' . $blockStyle . '.php'; ?>
 							</div>
 						</div>
-				        <?php
+				        <?
 				    endwhile; ?>
 				</div>
-				<?php endif;
+				<? endif;
 				wp_reset_query();
 			?>
-			<?php } elseif($blockType == 'featured'){
+			<? } elseif($blockType == 'featured'){
 				$args = array(
 				    'posts_per_page' => $blockCount,
 					'meta_key' => 'featured_post',
@@ -198,31 +145,27 @@
 				);
 				$featuredPost = new WP_Query($args); 
 				if($featuredPost->have_posts()): ?>
-				<div class="row <?php if($blockGutters):?>no-gutters<?php endif; ?>">
-				    <?php while($featuredPost->have_posts()): $featuredPost->the_post();
+				<div class="row <? if($blockGutters):?>no-gutters<? endif; ?>">
+				    <? while($featuredPost->have_posts()): $featuredPost->the_post();
 					    $postID = get_the_ID();
 					    $selectedItem = get_post($postID);
 				        ?>
-				        <div class="col-12 col-md-<?php echo $blockColumns; ?> multi-block__column">
-							<div class="multi-block__wrap-inner <?php echo $blockStyle . ' ' . $blockBGColor; ?>">
-								<?php include $templatePath . '/template-parts/kiss/flexi-partials/multi-block-layouts/block-' . $blockStyle . '.php'; ?>
+				        <div class="col-12 col-md-<? echo $blockColumns; ?> multi-block__column">
+							<div class="multi-block__wrap-inner <? echo $blockStyle . ' ' . $blockBGColor; ?>">
+								<? include $templatePath . '/template-parts/kiss/flexi-partials/multi-block-layouts/block-' . $blockStyle . '.php'; ?>
 							</div>
 						</div>
-				        <?php
+				        <?
 				    endwhile; ?>
 				</div>
-				<?php endif; ?>
-			<?php 
+				<? endif; ?>
+			<? 
 				wp_reset_query();
 				} ?>
-			<?php if($addButton){ ?>
-			<div class="list-icons__actions">
-				<a class="btn-custom <?php echo $btnColour; ?> <?php if($setLink=='form'): echo $linkClass; endif; ?>" href="<?php if($setLink=='email'):?>mailto:<?php endif; ?><?php echo $linkContent; ?>" <?php if($setLink=='link'):?>target="_blank"<?php endif; ?>><?php if($linkText){ echo $linkText; } else { ?>LEARN HOW<?php } ?></a>
-			</div>
-			<?php } ?>	
-		<?php if($blockContainer):?></div><?php endif; ?>
+			<? include $templatePartials . "add-button.php"; ?>	
+		<? if($blockContainer):?></div><? endif; ?>
 	</div>
-	<?php if($addSeparatorLower == true):
+	<? if($addSeparatorLower == true):
 		include $pathLower;
 	endif; ?>
 </section>
